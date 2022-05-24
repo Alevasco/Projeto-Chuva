b3883c56177c939ba5226bf788751fb716faf2b2
unknown <ale_vasconcelos96@hotmail.com>
2022-05-24 13:24:59 -0300

    Projeto Chuva

---
+++ script.js
@@ -0,0 +1,15 @@
+function verMais(){
+    let pontos=document.getElementById("pontos");
+    let maisTexto=document.getElementById("mais");
+    let btnVerMais=document.getElementById("btnVerMais");
+  
+    if (pontos.style.display === "none") {
+      pontos.style.display = "inline";
+      maisTexto.style.display = "none";
+      btnVerMais.innerHTML = "Ver mais";
+    } else {
+      pontos.style.display = "none";
+      maisTexto.style.display = "inline";
+      btnVerMais.innerHTML = "Ver menos";
+    }
+  }
